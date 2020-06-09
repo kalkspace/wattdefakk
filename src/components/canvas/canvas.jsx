79dbb1f79fb7e13bf9294c2286beba885fc6c7b5
P3nny <p3nny@hacking.studio>
2020-06-09 22:07:56 +0200

    ✨ Make it draggable

--- src/components/canvas/canvas.jsx
+++ src/components/canvas/canvas.jsx
@@ -1,20 +1,59 @@
 import React from "react";
 import "./canvas.scss";
+import Draggable from "react-draggable";
+
+const DragElement = ({ children }) => {
+  return (
+    <Draggable
+      // axis="x"
+      handle=".handle"
+      defaultPosition={{ x: 0, y: 0 }}
+      position={null}
+      //   grid={[25, 25]}
+      scale={1}
+      // onStart={this.handleStart}
+      // onDrag={this.handleDrag}
+      // onStop={this.handleStop}
+    >
+      <g className="handle">{children}</g>
+    </Draggable>
+  );
+};
 
 const Canvas = () => {
   return (
     <svg viewBox="0 0 220 220" xmlns="http://www.w3.org/2000/svg">
-      <rect width="2" height="10" x="30" y="200" />
-      <rect width="2" height="10" x="35" y="200" />
-      <rect width="2" height="10" x="40" y="200" />
-      <rect width="2" height="10" x="45" y="200" />
-      <rect width="2" height="10" x="50" y="200" />
+      <DragElement>
+        <rect width="2" height="10" x="30" y="200" />
+      </DragElement>
+      <DragElement>
+        <rect width="2" height="10" x="35" y="200" />
+      </DragElement>
+      <DragElement>
+        <rect width="2" height="10" x="40" y="200" />
+      </DragElement>
+      <DragElement>
+        <rect width="2" height="10" x="45" y="200" />
+      </DragElement>
+      <DragElement>
+        <rect width="2" height="10" x="50" y="200" />
+      </DragElement>
 
-      <rect width="2" height="20" x="55" y="200" />
-      <rect width="2" height="20" x="60" y="200" />
-      <rect width="2" height="20" x="65" y="200" />
-      <circle cx="75" cy="200" r="6" fill="orange" />
-      <circle cx="90" cy="200" r="6" fill="orange" />
+      <DragElement>
+        <rect width="2" height="20" x="55" y="200" />
+      </DragElement>
+      <DragElement>
+        <rect width="2" height="20" x="60" y="200" />
+      </DragElement>
+      <DragElement>
+        <rect width="2" height="20" x="65" y="200" />
+      </DragElement>
+      <DragElement>
+        <circle cx="75" cy="205" r="6" />
+      </DragElement>
+      <DragElement>
+        <circle cx="90" cy="205" r="6" />
+      </DragElement>
     </svg>
   );
 };
