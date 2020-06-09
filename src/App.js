8e007177e8e7016a25c010eb1678226b7bcddb8d
Jannes Höke <mail@jhoeke.de>
2020-06-09 23:15:40 +0200

    ✨ Add some routing and firebase login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,48 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from "react-router-dom";
+
 import "./App.css";
 
+import { firebaseApp } from './firebase/app';
+import { useAnonymousAuth } from './firebase/hooks';
 import Canvas from "./components/canvas/canvas";
 
 function App() {
+
+  const authState = useAnonymousAuth();
+
   return (
-    <div className="App">
-      <header>
-        <h1>Wattdefakk</h1>
-      </header>
-      <Canvas />
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <h1>Wattdefakk</h1>
+        </header>
+        {
+          authState.state === "success" ?
+            <Switch>
+              <Route path="/game/:uuid">
+                <Canvas />
+              </Route>
+              <Route path="/create">
+                <div>
+                  Create Game
+            </div>
+              </Route>
+              <Route path="/">
+                <div>
+                  Landing Page
+            </div>
+              </Route>
+            </Switch>
+            :
+            <div>Logging you in!</div>
+        }
+      </div>
+    </Router>
   );
 }
 
