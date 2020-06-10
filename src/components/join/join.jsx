729dc36f9631bedef4fc1e5f2234441d4006888e
Jannes Höke <mail@jhoeke.de>
2020-06-10 19:06:56 +0200

    ✨ Joining and leaving games

--- src/components/join/join.jsx
+++ src/components/join/join.jsx
@@ -1,26 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback, useContext } from "react";
 import { useParams } from "react-router-dom";
-import { useDocumentData } from "react-firebase-hooks/firestore";
+import {
+  useDocument,
+  useDocumentData,
+  useCollection,
+  useCollectionData,
+} from "react-firebase-hooks/firestore";
 
 import { fireUsers, fireGames } from "../../firebase/app";
+import { UserContext } from "../../contexts/user";
 
 const Join = ({ ...rest }) => {
+  const userContext = useContext(UserContext);
   const { id } = useParams();
-  const [game, gameLoading, gameError] = useDocumentData(fireGames.doc(id));
+
+  const [game, gameLoading, gameError] = useDocument(fireGames.doc(id));
   const [owner, ownerLoading, ownerError] = useDocumentData(
-    game ? fireUsers.doc(game.owner) : undefined
+    game && fireUsers.doc(game.get("owner"))
   );
+  const [players, playersLoading, playersError] = useCollection(
+    game && game.ref.collection("players")
+  );
+
+  const joinCallback = useCallback(() => {
+    game.ref.collection("players").doc(userContext.user.uid).set({});
+  }, [game, userContext]);
+  const leaveCallback = useCallback(() => {
+    game.ref.collection("players").doc(userContext.user.uid).delete();
+  }, [game, userContext]);
 
   return (
     <div {...rest}>
       <h2>
         Joining game {id} by {owner ? owner.name : "..."}
       </h2>
+      Players:
+      <ul>
+        {players && players.docs.map((player) => <li>{player.ref.id}</li>)}
+      </ul>
       <input
         type="text"
         value={window.origin + `/join/${id}`}
         readOnly={true}
       />
+      <button onClick={joinCallback}>Join game</button>
+      <button onClick={leaveCallback}>Leave game</button>
     </div>
   );
 };
