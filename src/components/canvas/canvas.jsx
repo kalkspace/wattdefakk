ec54ee332274f9a3a151482ca40cb366a33a32d3
paqe <4215065+paqe@users.noreply.github.com>
2020-06-16 21:39:52 +0200

    Store piece position in state

--- src/components/canvas/canvas.jsx
+++ src/components/canvas/canvas.jsx
@@ -2,18 +2,13 @@ import React from "react";
 import "./canvas.scss";
 import Draggable from "react-draggable";
 
-const DragElement = ({ children }) => {
+const DragElement = ({ onStop, position, children }) => {
   return (
     <Draggable
-      // axis="x"
       handle=".handle"
-      defaultPosition={{ x: 0, y: 0 }}
-      position={null}
-      //   grid={[25, 25]}
+      position={position}
       scale={1}
-      // onStart={this.handleStart}
-      // onDrag={this.handleDrag}
-      // onStop={this.handleStop}
+      onStop={onStop}
     >
       <g className="handle">{children}</g>
     </Draggable>
@@ -21,40 +16,55 @@ const DragElement = ({ children }) => {
 };
 
 const Canvas = () => {
-  return (
-    <svg viewBox="0 0 220 220" xmlns="http://www.w3.org/2000/svg">
-      <DragElement>
-        <rect width="2" height="10" x="30" y="200" />
-      </DragElement>
-      <DragElement>
-        <rect width="2" height="10" x="35" y="200" />
-      </DragElement>
-      <DragElement>
-        <rect width="2" height="10" x="40" y="200" />
-      </DragElement>
-      <DragElement>
-        <rect width="2" height="10" x="45" y="200" />
-      </DragElement>
-      <DragElement>
-        <rect width="2" height="10" x="50" y="200" />
-      </DragElement>
+  const [piece, setPiece] = React.useState({ x: 0, y: 0 });
 
-      <DragElement>
-        <rect width="2" height="20" x="55" y="200" />
-      </DragElement>
-      <DragElement>
-        <rect width="2" height="20" x="60" y="200" />
-      </DragElement>
-      <DragElement>
-        <rect width="2" height="20" x="65" y="200" />
-      </DragElement>
-      <DragElement>
-        <circle cx="75" cy="205" r="6" />
-      </DragElement>
-      <DragElement>
-        <circle cx="90" cy="205" r="6" />
-      </DragElement>
-    </svg>
+  const handleStop = (_event, position) => {
+    const { x, y } = position;
+    setPiece({ x, y });
+  };
+
+  return (
+    <>
+      <svg viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
+        <DragElement position={{ x: piece.x, y: piece.y}} onStop={handleStop}>
+          <rect
+            width="5"
+            height="50"
+            x={30}
+            y={400}
+          />
+        </DragElement>
+
+        {/* <DragElement>
+          <rect width="2" height="10" x="35" y="200" />
+        </DragElement>
+        <DragElement>
+          <rect width="2" height="10" x="40" y="200" />
+        </DragElement>
+        <DragElement>
+          <rect width="2" height="10" x="45" y="200" />
+        </DragElement>
+        <DragElement>
+          <rect width="2" height="10" x="50" y="200" />
+        </DragElement> */}
+
+        {/* <DragElement>
+          <rect width="2" height="20" x="55" y="200" />
+        </DragElement>
+        <DragElement>
+          <rect width="2" height="20" x="60" y="200" />
+        </DragElement>
+        <DragElement>
+          <rect width="2" height="20" x="65" y="200" />
+        </DragElement>
+        <DragElement>
+          <circle cx="75" cy="205" r="6" />
+        </DragElement>
+        <DragElement>
+          <circle cx="90" cy="205" r="6" />
+        </DragElement> */}
+      </svg>
+    </>
   );
 };
 
