ea2f32d3a3a98ee87b2b5d78787f57c99a5d4f1e
christinegotth <christine.gotth@gmail.com>
2020-07-21 22:59:07 +0200

    Enable multiple sticks

--- src/components/canvas/canvas.jsx
+++ src/components/canvas/canvas.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./canvas.scss";
 import Draggable from "react-draggable";
 import { useParams } from "react-router-dom";
-import { useDocument } from "react-firebase-hooks/firestore";
+import { useDocument, useCollection } from "react-firebase-hooks/firestore";
 import { fireGames } from "../../firebase/app";
 
 const DragElement = ({ onStop, position, children }) => {
   const nodeRef = React.useRef(null);
   return (
-    <Draggable nodeRef={nodeRef} handle=".handle" position={position} scale={1} onStop={onStop}>
-      <g ref={nodeRef} className="handle">{children}</g>
+    <Draggable
+      nodeRef={nodeRef}
+      handle=".handle"
+      position={position}
+      scale={1}
+      onStop={onStop}
+    >
+      <g ref={nodeRef} className="handle">
+        {children}
+      </g>
     </Draggable>
   );
 };
@@ -18,17 +26,34 @@ const Canvas = () => {
   const { id } = useParams();
 
   const [game, gameLoading, gameError] = useDocument(fireGames.doc(id));
-
-  const moveCallback = React.useCallback(
-    ({ x, y }) => {
-      game.ref.update({ x, y });
-    },
-    [game],
+  const roundsRef = game?.ref.collection("rounds");
+  const [rounds, roundsLoading, roundsError] = useCollection(
+    roundsRef?.orderBy("num", "desc")
   );
+
+  const currentRound = rounds?.docs?.[0];
+
+  useEffect(() => {
+    if (roundsRef && rounds?.empty) {
+      roundsRef.add({
+        num: 1,
+        x: 35,
+        y: 200,
+      });
+    }
+  }, [roundsRef, rounds]);
 
   const handleStop = (_event, position) => {
     const { x, y } = position;
-    moveCallback({ x, y });
+    currentRound.ref.update({ x, y });
+  };
+
+  const nextRound = () => {
+    roundsRef.add({
+      num: currentRound.get("num") + 1,
+      x: 35,
+      y: 200,
+    });
   };
 
   if (!game) {
@@ -39,42 +64,20 @@ const Canvas = () => {
     <>
       {/* {JSON.stringify(game.data())}<br/> */}
       <svg viewBox="0 0 440 440" xmlns="http://www.w3.org/2000/svg">
-        <DragElement
-          position={{ x: game.get("x") ?? 0, y: game.get("y") ?? 0 }}
-          onStop={handleStop}
-        >
-          <rect width="5" height="50" x={30} y={400} />
-        </DragElement>
-
-        {/* <DragElement>
-          <rect width="2" height="10" x="35" y="200" />
-        </DragElement>
-        <DragElement>
-          <rect width="2" height="10" x="40" y="200" />
-        </DragElement>
-        <DragElement>
-          <rect width="2" height="10" x="45" y="200" />
-        </DragElement>
-        <DragElement>
-          <rect width="2" height="10" x="50" y="200" />
-        </DragElement> */}
-
-        {/* <DragElement>
-          <rect width="2" height="20" x="55" y="200" />
-        </DragElement>
-        <DragElement>
-          <rect width="2" height="20" x="60" y="200" />
-        </DragElement>
-        <DragElement>
-          <rect width="2" height="20" x="65" y="200" />
-        </DragElement>
-        <DragElement>
-          <circle cx="75" cy="205" r="6" />
-        </DragElement>
-        <DragElement>
-          <circle cx="90" cy="205" r="6" />
-        </DragElement> */}
+        {rounds?.docs.map((snap) =>
+          snap.id === currentRound.id ? (
+            <DragElement
+              position={{ x: snap.get("x") ?? 0, y: snap.get("y") ?? 0 }}
+              onStop={handleStop}
+            >
+              <rect width="5" height="50" x={0} y={0} className="active-rect" />
+            </DragElement>
+          ) : (
+            <rect width="5" height="50" x={snap.get("x")} y={snap.get("y")} />
+          )
+        )}
       </svg>
+      <button onClick={nextRound}>Weiter</button>
     </>
   );
 };
